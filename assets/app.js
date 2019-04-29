cf9df03ec135de3c6db63073893cdacd91cb9e82
Petr Nagy <petr.nagy@outlook.com>
2019-04-29 19:29:41 +0800

    template edits

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,15 @@
 /*
     Hello world !!!
 */
-paint();
-init_img_lazy_loading();
+
+init();
+
+function init() {
+    paint();
+    window.addEventListener('load', e => {
+        document.body.classList.add( Browser.isMobileDevice() ? 'is-mobile' : 'is-desktop' );
+    });
+} // end func
 
 function paint() {
     let list = document.getElementsByClassName('_job-pls');
@@ -21,7 +28,7 @@ function draw_job_card(el) {
     if (!el.dataset.studyresource) el.dataset.studyresource = 'https://www.google.com/search?q=' + encodeURIComponent('How to be ' + el.dataset.title);
     if (!el.dataset.jobboard) el.dataset.jobboard = 'https://www.google.com/search?q=' + encodeURIComponent('Job boards for ' + el.dataset.title);
 
-    o += '<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 _job _img-hover-zoom--quick-zoom">';
+    o += '<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 _job _img-hover-zoom--quick-zoom _card-wrapper">';
     o += '    <div class="card">';
     o += '        <div class="card-body _card-body">';
     o += '            <h3 class="h4 card-title">' + el.dataset.title + '</h3>';
@@ -29,7 +36,7 @@ function draw_job_card(el) {
     o += '            <span title="Difficulty to learn" class="_label-diff ' + el.dataset.difficulty + '">' + el.dataset.difficulty + '</span>';
     o += '        </div>';
     o += '        <div class="_img-wrapper">';
-    o += '            <img class="photo _photo" src="'+placeholder+'" lazy-src="assets/img/jobs/w348/' + img + '--w348.jpg" lazy-srcset="assets/img/jobs/w348/' + img + '--w348.jpg 348w, assets/img/jobs/w696/' + img + '--w696.jpg 696w, assets/img/jobs/w1044/' + img + '--w1044.jpg 1044w" alt="Picture" />';
+    o += '            <img class="photo _photo" src="assets/img/jobs/w348/' + img + '--w348.jpg" srcset="assets/img/jobs/w348/' + img + '--w348.jpg 348w, assets/img/jobs/w696/' + img + '--w696.jpg 696w, assets/img/jobs/w1044/' + img + '--w1044.jpg 1044w" alt="Picture" />';
     o += '        </div>';
     o += '        <div class="card-body">';
     o += '            <a href="' + el.dataset.studyresource + '" target="_blank" rel="nofollow" class="card-link _card-link text-success">Study materials <img class="_external" src="assets/img/open-in-new.svg" alt="Continue" /></a>';
@@ -75,30 +82,6 @@ function scrollTo(to, duration) {
     animateScroll();
 } // end func
 
-function init_img_lazy_loading() {
-    var timeout = null;
-    var lastOffset = 0;
-    window.addEventListener('scroll', e => {
-        clearTimeout(timeout);
-        lastOffset = document.body.offsetTop;
-        timeout = setTimeout(() => {
-            if ( lastOffset === document.body.offsetTop ) {
-                lazy_load_imgs();
-            } // end if
-        }, 100);
-    });
-};
-
-function lazy_load_imgs() {
-    let list = document.getElementsByClassName('_photo');
-    Array.from(list).forEach(el => {
-        if ( is_in_viewport(el) ) {
-            el.setAttribute('src', el.getAttribute('lazy-src'));
-            el.setAttribute('srcset', el.getAttribute('lazy-srcset'));
-        } // end if
-    });
-} // end func
-
 function is_in_viewport(el) {
     var bounding = el.getBoundingClientRect();
     if ( bounding.top >= document.body.scrollTop && bounding.top <= (document.body.scrollTop + window.innerHeight) ) {
@@ -118,3 +101,105 @@ function slugify(text) {
         .replace(/^-+/, '')             // Trim - from start of text
         .replace(/-+$/, '');            // Trim - from end of text
 } // end func
+
+/**
+* @author PN @since 2015-05-01
+*/
+Browser = {
+
+    /**
+    * @static
+    * @return Boolean
+    */
+    isEdge: function (){
+        return navigator.userAgent.match(/Edge\/\d./i);
+    }, // end method
+
+    /**
+    * @static
+    * @return Boolean
+    */
+    isExplorer: function (){
+        return ( navigator.userAgent.indexOf('MSIE') > -1 );
+    }, // end method
+
+    /**
+    * @static
+    * @return Boolean
+    */
+    isChrome: function (){
+        var isChrome = ( navigator.userAgent.indexOf('Chrome') > -1 );
+        return ( isChrome && ! Browser.isOpera() );
+    }, // end method
+
+    /**
+    * @static
+    * @return Boolean
+    */
+    isSafari: function (){
+        var isSafari = ( navigator.userAgent.indexOf("Safari") > -1 );
+        return ( isSafari && ! Browser.isChrome() && ! Browser.isOpera() );
+    }, // end method
+
+    /**
+    * @static
+    * @return Boolean
+    */
+    isFirefox: function (){
+        return ( navigator.userAgent.indexOf('Firefox') > -1 );
+    }, // end method
+
+    /**
+    * @static
+    * @return Boolean
+    */
+    isOpera: function (){
+        return ( navigator.userAgent.toLowerCase().indexOf("opr") > -1 );
+    }, // end func
+
+    /**
+    * @static
+    * @return Boolean
+    */
+    isIos: function() {
+        if ( navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) ) {
+            if ( ! Browser.isWindowsPhone() ) {
+                return true;
+            } // end if
+        } // end if
+        return false;
+    }, // end method
+
+    /**
+    * @static
+    * @return Boolean
+    */
+    isAndroid: function() {
+        return ( navigator.userAgent.toLowerCase().indexOf("android") > -1 && ! Browser.isWindowsPhone() );
+    }, // end method
+
+    /**
+    * @static
+    * @return Boolean
+    */
+    isWindowsPhone: function() {
+        return navigator.userAgent.match(/Windows Phone/i);
+    }, // end method
+
+    /**
+    * @static
+    * @return Boolean
+    */
+    isMobileDevice: function() {
+        return ( Browser.isIos() || Browser.isAndroid() || Browser.isWindowsPhone() );
+    }, // end method
+
+    /**
+    * @static
+    * @return Boolean
+    */
+    isDesktopDevice: function() {
+        return ! Browser.isMobileDevice();
+    }, // end method
+
+};
