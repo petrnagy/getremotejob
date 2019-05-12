6aa21b7ff95c6498009757ded4688f12e1bf572c
Petr Nagy <petr.nagy@outlook.com>
2019-05-12 13:03:04 +0800

    continuous development

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,8 @@
 /*
-    Hello world !!!
+    Hello bitches !!!
+    License: MIT
+    Author: Petr Nagy (https://twitter.com/petrnagy)
+    More at https://getafuckingremotejob.xyz/readme.md
 */
 
 init();
@@ -9,47 +12,60 @@ function init() {
     window.addEventListener('load', e => {
         document.body.classList.add( Browser.isMobileDevice() ? 'is-mobile' : 'is-desktop' );
     });
+
+    var scrollTimer = null;
+    window.addEventListener('scroll', e => {
+        clearTimeout(scrollTimer);
+        scrollTimer = setTimeout(() => {
+            let height = window.innerHeight;
+            let scrolled = window.pageYOffset;
+            if ( scrolled >= height ) {
+                document.body.classList.add('_is-scrolled');
+            } else {
+                document.body.classList.remove('_is-scrolled');
+            } // end if
+        }, 25);
+    });
+
+    let hash = window.location.hash.replace('#', '');
+    if ( hash.length > 1 ) {
+        setTimeout(() => {
+            scroll_to_id(hash);
+        }, 500);
+    } // end if
 } // end func
 
 function paint() {
+    let counter = 0;
     let list = document.getElementsByClassName('_job-pls');
     Array.from(list).forEach(item => {
-        item.outerHTML = draw_job_card(item);
+        ++counter;
+        item.outerHTML = draw_job_card(item, counter);
     });
 } // end func
 
-function draw_job_card(el) {
+function draw_job_card(el, index) {
     let o = '';
-    let placeholder = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzMiAzMiIgd2lkdGg9IjMyIiBoZWlnaHQ9IjMyIiBmaWxsPSJ3aGl0ZSI+CiAgPHBhdGggZD0iTTAgNCBMMCAyOCBMMzIgMjggTDMyIDQgeiBNNCAyNCBMMTAgMTAgTDE1IDE4IEwxOCAxNCBMMjQgMjR6IE0yNSA3IEE0IDQgMCAwIDEgMjUgMTUgQTQgNCAwIDAgMSAyNSA3Ij48L3BhdGg+Cjwvc3ZnPg==';
-
-    let img = slugify(el.dataset.title);
+    let id = 'row--' + index;
 
     if (!el.dataset.desc) el.dataset.desc = 'Nulla feugiat aliquet feugiat.';
-    if (!el.dataset.studyresource) el.dataset.studyresource = 'https://www.google.com/search?q=' + encodeURIComponent('How to be ' + el.dataset.title);
-    if (!el.dataset.jobboard) el.dataset.jobboard = 'https://www.google.com/search?q=' + encodeURIComponent('Job boards for ' + el.dataset.title);
+    if (!el.dataset.studyresource) el.dataset.studyresource = 'https://www.google.com/search?q=' + encodeURIComponent('How do I become ' + el.dataset.title);
 
-    o += '<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 _job _img-hover-zoom--quick-zoom _card-wrapper">';
-    o += '    <div class="card">';
-    o += '        <div class="card-body _card-body">';
-    o += '            <h3 class="h4 card-title">' + el.dataset.title + '</h3>';
-    o += '            <p class="card-text text-muted">' + el.dataset.desc + '</p>';
-    o += '            <span title="Difficulty to learn" class="_label-diff ' + el.dataset.difficulty + '">' + el.dataset.difficulty + '</span>';
-    o += '        </div>';
-    o += '        <div class="_img-wrapper">';
-    o += '            <img class="photo _photo" src="assets/img/jobs/w348/' + img + '--w348.jpg" srcset="assets/img/jobs/w348/' + img + '--w348.jpg 348w, assets/img/jobs/w696/' + img + '--w696.jpg 696w, assets/img/jobs/w1044/' + img + '--w1044.jpg 1044w" alt="Picture" />';
-    o += '        </div>';
-    o += '        <div class="card-body">';
-    o += '            <a href="' + el.dataset.studyresource + '" target="_blank" rel="nofollow" class="card-link _card-link text-success">Study materials <img class="_external" src="assets/img/open-in-new.svg" alt="Continue" /></a>';
-    o += '            <a href="' + el.dataset.jobboard + '" target="_blank" rel="nofollow" class="card-link _card-link text-success">Job board <img class="_external" src="assets/img/open-in-new.svg" alt="Continue" /></a>';
-    o += '        </div>';
-    o += '    </div>';
-    o += '</div>';
+    o += '<tr onclick="open_window(event, \''+el.dataset.studyresource+'\');" class="_job _pointer">';
+    o += '    <th class="_name _first text-left" scope="row"><div id="'+id+'" class="_relative"><a onclick="return scroll_to_id(\''+id+'\');" href="#'+id+'" class="_anchor">#</a>' + el.dataset.title + '</div></th>';
+    o += '    <td class="_title _second text-left"><div class="_desc _text-gray">' + el.dataset.desc + '</div></td>';
+    o += '    <td class="_badge _third text-left"><span title="How fast can you approximately start" class="badge _label-diff ' + el.dataset.difficulty + '">' + el.dataset.difficulty + '</span></td>';
+    o += '    <td class="_links _fourth text-right"><a href="' + el.dataset.studyresource + '" target="_blank" rel="nofollow noopener" class="text-success _link">Start <img class="_external" src="assets/img/open-in-new.svg" alt="Continue" /></a></td>';
+    o += '</tr>';
+
     return o;
 } // end func
 
 function scroll_to_id(id) {
-    let offset = document.getElementById(id).offsetTop;
+    let menuHeight = 70;
+    let offset = document.getElementById(id).offsetTop - menuHeight;
     scrollTo(offset, 500);
+    window.location.hash = '#' + id;
     return false;
 }
 
@@ -100,6 +116,14 @@ function slugify(text) {
         .replace(/\-\-+/g, '-')         // Replace multiple - with single -
         .replace(/^-+/, '')             // Trim - from start of text
         .replace(/-+$/, '');            // Trim - from end of text
+} // end func
+
+function open_window(e, link) {
+    e = e || window.event;
+    let target = e.target || e.srcElement;
+    if ( ! target.classList.contains('_anchor') ) {
+        window.open(link, '_blank');
+    } // end if
 } // end func
 
 /**
